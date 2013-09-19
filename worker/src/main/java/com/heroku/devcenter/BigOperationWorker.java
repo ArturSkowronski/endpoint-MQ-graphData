f0a653ef1f198d11d6d0b12d4f7c40318dfabae9
Articles <articles.pl@gmail.com>
2013-09-19 18:40:51 +0200

    Retrieving Message Path

--- worker/src/main/java/com/heroku/devcenter/BigOperationWorker.java
+++ worker/src/main/java/com/heroku/devcenter/BigOperationWorker.java
@@ -11,6 +11,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.util.ErrorHandler;
 
+import static java.lang.System.getenv;
+
 /**
  * Worker for receiving and processing BigOperations asynchronously.
  */
@@ -34,7 +36,7 @@ public class BigOperationWorker {
                 final BigOperation bigOp = (BigOperation) messageConverter.fromMessage(message);
 
                 // simply printing out the operation, but expensive computation could happen here
-                System.out.println("Received from RabbitMQ: " + bigOp);
+                System.out.println("Received from RabbitMQ "+getenv("CLOUDAMQP_URL")+": " + bigOp);
             }
         });
 
