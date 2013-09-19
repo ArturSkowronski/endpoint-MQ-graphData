78831f0d555493f4c02d508aafd0b0190ce5eec2
Articles <articles.pl@gmail.com>
2013-09-19 23:06:17 +0200

    Add Redis Store

--- worker/src/main/java/com/heroku/devcenter/BigOperationWorker.java
+++ worker/src/main/java/com/heroku/devcenter/BigOperationWorker.java
@@ -10,31 +10,59 @@ import org.springframework.amqp.support.converter.SimpleMessageConverter;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.util.ErrorHandler;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.Protocol;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Set;
 
 /**
  * Worker for receiving and processing BigOperations asynchronously.
  */
 public class BigOperationWorker {
+    static JedisPool pool;
 
     public static void main(String[] args) {
+
         final ApplicationContext rabbitConfig = new AnnotationConfigApplicationContext(RabbitConfiguration.class);
         final ConnectionFactory rabbitConnectionFactory = rabbitConfig.getBean(ConnectionFactory.class);
         final Queue rabbitQueue = rabbitConfig.getBean(Queue.class);
         final MessageConverter messageConverter = new SimpleMessageConverter();
-
         // create a listener container, which is required for asynchronous message consumption.
         // AmqpTemplate cannot be used in this case
         final SimpleMessageListenerContainer listenerContainer = new SimpleMessageListenerContainer();
         listenerContainer.setConnectionFactory(rabbitConnectionFactory);
         listenerContainer.setQueueNames(rabbitQueue.getName());
-
+        try {
+            URI redisURI = new URI(System.getenv("REDISTOGO_URL"));
+            pool = new JedisPool(new JedisPoolConfig(),
+                    redisURI.getHost(),
+                    redisURI.getPort(),
+                    Protocol.DEFAULT_TIMEOUT,
+                    redisURI.getUserInfo().split(":",2)[1]);
+        } catch (URISyntaxException e) {
+            // URI couldn't be parsed. Handle exception
+        }
         // set the callback for message handling
+        final JedisPool finalPool = pool;
         listenerContainer.setMessageListener(new MessageListener() {
             public void onMessage(Message message) {
                 final DataSimulation bigOp = (DataSimulation) messageConverter.fromMessage(message);
 
-                // simply printing out the operation, but expensive computation could happen here
+
                 System.out.println("Received from RabbitMQ : " + bigOp);
+                Jedis jedis = finalPool.getResource();
+                try {
+                    jedis.set("foo", "bar");
+                    String foobar = jedis.get("foo");
+                    jedis.zadd("sose", 0, "car"); jedis.zadd("sose", 0, "bike");
+                    Set<String> sose = jedis.zrange("sose", 0, -1);
+                } finally {
+                    finalPool.returnResource(jedis);
+                }
             }
         });
 
